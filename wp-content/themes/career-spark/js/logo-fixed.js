a7ca1dd208c750e3571e8713256b706e6b20dee1
Craig Morey <craig.morey@uswitch.com>
2013-03-11 11:43:01 +0000

    make red background bigger, fixed logo goes green in footer

--- wp-content/themes/career-spark/js/logo-fixed.js
+++ wp-content/themes/career-spark/js/logo-fixed.js
@@ -21,24 +21,24 @@ $(document).ready( function() {
       $('#logo-back2').addClass('red').css({'z-index' : 1, '-webkit-mask-position': '0 -' + ( 60 + top ) + 'px'});
     // red section - top
     } else if ( redTop.length > 0 && top >= (redTopOff - 160) && top < redTopOff ) {
-      $('#logo-back1').addClass('yellow').css({'z-index' : 1, '-webkit-mask-position': '0 -' + ( 227 + (top - redTopOff) ) + 'px'});
-      $('#logo-back2').css({'z-index' : 0, '-webkit-mask-position': '0 -250px'});
+      $('#logo-back1').removeClass('green').addClass('yellow').css({'z-index' : 1, '-webkit-mask-position': '0 -' + ( 227 + (top - redTopOff) ) + 'px'});
+      $('#logo-back2').addClass('red').css({'z-index' : 0, '-webkit-mask-position': '0 -250px'});
     // red section - within
     } else if ( redTop.length > 0 && top > redTopOff && top < redBottomOff - 160 ) {
-      $('#logo-back1').addClass('yellow').css({'z-index' : 1, '-webkit-mask-position': '0 -250px'});
-      $('#logo-back2').css({'z-index' : 0, '-webkit-mask-position': '0 0'});
+      $('#logo-back1').removeClass('green').addClass('yellow').css({'z-index' : 1, '-webkit-mask-position': '0 -250px'});
+      $('#logo-back2').addClass('red').css({'z-index' : 0, '-webkit-mask-position': '0 0'});
     // red section - bottom
     } else if ( redTop.length > 0 && top >= (redBottomOff - 160) && top < (redBottomOff + 80) ) {
-      $('#logo-back1').addClass('yellow').css({'z-index' : 0, '-webkit-mask-position': '0 -250px'});
-      $('#logo-back2').addClass('red').css({'z-index' : 1, '-webkit-mask-position': '0 -' + ( 146 + (top - redBottomOff) ) + 'px'});
+      $('#logo-back1').removeClass('green').addClass('yellow').css({'z-index' : 0, '-webkit-mask-position': '0 -250px'});
+      $('#logo-back2').addClass('red').addClass('red').css({'z-index' : 1, '-webkit-mask-position': '0 -' + ( 146 + (top - redBottomOff) ) + 'px'});
     // footer - top
     } else if ( top >= (footerTopOff - 170) && top < footerTopOff ) {
-      $('#logo-back1').addClass('yellow').css({'z-index' : 1, '-webkit-mask-position': '0 -' + ( 227 + (top - footerTopOff) ) + 'px'});
-      $('#logo-back2').css({'z-index' : 0, '-webkit-mask-position': '0 -250px'});
+      $('#logo-back1').removeClass('yellow').addClass('green').css({'z-index' : 1, '-webkit-mask-position': '0 -' + ( 224 + (top - footerTopOff) ) + 'px'});
+      $('#logo-back2').addClass('red').css({'z-index' : 0, '-webkit-mask-position': '0 -250px'});
     // red section - within
     } else if ( top > footerTopOff && top < 999999999 ) {
-      $('#logo-back1').addClass('yellow').css({'z-index' : 1, '-webkit-mask-position': '0 -250px'});
-      $('#logo-back2').css({'z-index' : 0, '-webkit-mask-position': '0 0'});
+      $('#logo-back1').removeClass('yellow').addClass('green').css({'z-index' : 1, '-webkit-mask-position': '0 -250px'});
+      $('#logo-back2').addClass('red').css({'z-index' : 0, '-webkit-mask-position': '0 0'});
     // default
     } else {
       $('#logo-back1').attr('class','').css({'z-index' : 0});
