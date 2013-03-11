070e3c63e200266bf0ae3253911f806b5e8fd4f3
Craig Morey <craig.morey@uswitch.com>
2013-03-11 04:50:34 +0000

    correct logo cut-off points

--- wp-content/themes/career-spark/js/logo-fixed.js
+++ wp-content/themes/career-spark/js/logo-fixed.js
@@ -21,7 +21,7 @@ $(document).ready( function() {
       $('#logo-back2').addClass('red').css({'z-index' : 1, '-webkit-mask-position': '0 -' + ( 60 + top ) + 'px'});
     // red section - top
     } else if ( redTop.length > 0 && top >= (redTopOff - 160) && top < redTopOff ) {
-      $('#logo-back1').addClass('yellow').css({'z-index' : 1, '-webkit-mask-position': '0 -' + ( 255 + (top - redTopOff) ) + 'px'});
+      $('#logo-back1').addClass('yellow').css({'z-index' : 1, '-webkit-mask-position': '0 -' + ( 227 + (top - redTopOff) ) + 'px'});
       $('#logo-back2').css({'z-index' : 0, '-webkit-mask-position': '0 -250px'});
     // red section - within
     } else if ( redTop.length > 0 && top > redTopOff && top < redBottomOff - 160 ) {
@@ -30,10 +30,10 @@ $(document).ready( function() {
     // red section - bottom
     } else if ( redTop.length > 0 && top >= (redBottomOff - 160) && top < redBottomOff ) {
       $('#logo-back1').css({'z-index' : 0, '-webkit-mask-position': '0 -250px'});
-      $('#logo-back2').addClass('red').css({'z-index' : 1, '-webkit-mask-position': '0 -' + ( 175 + (top - redBottomOff) ) + 'px'});
+      $('#logo-back2').addClass('red').css({'z-index' : 1, '-webkit-mask-position': '0 -' + ( 146 + (top - redBottomOff) ) + 'px'});
     // footer - top
     } else if ( top >= (footerTopOff - 160) && top < footerTopOff ) {
-      $('#logo-back1').addClass('yellow').css({'z-index' : 1, '-webkit-mask-position': '0 -' + ( 252 + (top - footerTopOff) ) + 'px'});
+      $('#logo-back1').addClass('yellow').css({'z-index' : 1, '-webkit-mask-position': '0 -' + ( 227 + (top - footerTopOff) ) + 'px'});
       $('#logo-back2').css({'z-index' : 0, '-webkit-mask-position': '0 -250px'});
     // red section - within
     } else if ( top > footerTopOff && top < 999999999 ) {
